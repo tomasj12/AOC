0cda5b9e72ce646a897a4804f275700fed1d441d
tomas.jurica <tomas.jurica@datasentics.com>
2022-06-25 23:58:07 +0200

    Add winning solution

--- Event_2/python/event_2.py
+++ Event_2/python/event_2.py
@@ -2,7 +2,7 @@ import typing as t
 import abc
 from pathlib import Path
 import collections.abc as tc
-
+import itertools as it
 
 ### Types
 Depth = t.TypeVar('Depth', bound=int)
@@ -25,17 +25,22 @@ class InputHandler(abc.ABC):
         pass
     
 
-
 class EventTwo(InputHandler):
 
      def __init__(self, path: Path) -> None:
          super().__init__(path)
          self.mapping = {'forward': 1, 'down': 1,'up':-1}
      
-     def handle_input(self, slinding_window: bool=False) -> t.Tuple[t.Sequence[Depth],t.Sequence[Height]]:
-         
-        depth = [self.mapping[d[0]] * int(d[1])  if d[0] != 'forward' else 0 for d in self.input]
+     def handle_input(self, aim_: bool=False) -> t.Tuple[t.Sequence[Depth],t.Sequence[Height]]:
+        
         height = [int(d[1]) if d[0] == 'forward' else 0 for d in self.input]
+
+        if aim_:
+            aim = [self.mapping[d[0]] * int(d[1])  if d[0] != 'forward' else 0 for d in self.input]
+            aim = list(it.accumulate(aim))
+            depth = [h * a for h,a in zip(height,aim)]
+        else:
+            depth = [self.mapping[d[0]] * int(d[1])  if d[0] != 'forward' else 0 for d in self.input]
 
         return depth, height
     
--- Event_2/python/main.py
+++ Event_2/python/main.py
@@ -7,5 +7,5 @@ PATH_DATA = PATH_REPO / 'Event_2' / 'Data' /'data.txt'
 if __name__ == '__main__':
 
     init = EventTwo(PATH_DATA)
-    depth,height = init.handle_input(slinding_window=True)
+    depth,height = init.handle_input(aim_=True)
     print(sum(depth) * sum(height))
